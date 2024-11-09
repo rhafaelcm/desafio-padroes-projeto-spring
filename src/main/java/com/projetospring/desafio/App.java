53c0052c1801e5ffe867c8a5087dfe7203b982de
rhafaelcm <rhafaelcm@msn.com>
2024-11-09 15:53:33 -0400

    first commit

---
+++ src/main/java/com/projetospring/desafio/App.java
@@ -0,0 +1,16 @@
+package com.projetospring.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Projeto para o desafio dio
+ * @author Rhafael da Costa Martins
+ */
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetospring/desafio/DesafioPadroesProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetospring.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPadroesProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
