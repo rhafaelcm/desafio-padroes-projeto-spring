f33254a5049148588e3f55778779665f0b05da53
rhafaelcm <rhafaelcm@msn.com>
2024-11-09 15:59:57 -0400

    add: Readme and OpenAPI/Swagger dependency

---
+++ README.md
@@ -0,0 +1,7 @@
+# Desafio Padrões de Projetos na Prática com Java
+
+- Spring Data JPA
+- Spring Web
+- H2 Database
+- OpenFeign
+- OpenAPI/Swagger
--- src/main/java/com/projetospring/desafio/App.java
+++ src/main/java/com/projetospring/desafio/App.java
@@ -2,10 +2,12 @@ package com.projetospring.desafio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 /**
  * Projeto para o desafio dio
  * @author Rhafael da Costa Martins
  */
+@EnableFeignClients
 @SpringBootApplication
 public class App {
 
